85308cb2a45396bceb8d18435039b37c0ba7b372
Android Wonder <jim.pepe@verizon.net>
2021-01-29 14:07:27 -0500

    Initial commit

---
+++ app/src/main/java/com/course/example/colorsfonts/MainActivity.java
@@ -0,0 +1,30 @@
+package com.course.example.colorsfonts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_main);
+
+        // bind the "custom" TextView with chosen font
+        TextView tvCustom=(TextView)findViewById(R.id.custom);
+        Typeface typeface = getResources().getFont(R.font.jokerman);
+        tvCustom.setTypeface(typeface);
+
+        //set color from /res/values/
+        int color = getResources().getColor(R.color.red, null);
+        tvCustom.setTextColor(color);
+
+        //set type size from /res/values/
+        float size = getResources().getDimension(R.dimen.customsize);
+        tvCustom.setTextSize(size);
+
+    }
+}
